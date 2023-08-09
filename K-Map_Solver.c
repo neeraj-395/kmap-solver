79643c513850a6e7edce3696c28d820b8efc6114
@Neeraj395-N3rd <nsinghh.04@gmail.com>
2023-08-09 22:45:56 +0530

    2-Variable K-Map Solver designed without the minimized expression

--- K-Map_Solver.c
+++ K-Map_Solver.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <conio.h>
-int main(){
-  //code will we added soon..
-}
+#include <string.h>
+void Expression(char *p,char *r){
+  if(*p == '1'){
+   strcpy(r,"A' ^ B' + ");
+   r+=10;
+  }
+  p++;
+  if(*p == '1'){
+    strcpy(r,"A ^ 'B + ");
+   r+=10;
+  }
+  p++;
+  if(*p == '1'){
+    strcpy(r,"A ^ B' + ");
+   r+=10;
+  }
+  p++;
+  if(*p == '1'){
+    strcpy(r,"A ^ B ");
+  }
+  };
+void main(){
+char Y[2][2]={{'1','0'},{'0','1'}},Result[30];
+Expression(&Y[0][0],&Result[0]);
+puts(Result);
 getch();
-return 0;
+}
 
