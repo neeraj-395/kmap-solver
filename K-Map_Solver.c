c8100114ff151f075b92b325ac25f213e246c412
@Neeraj395-N3rd <nsinghh.04@gmail.com>
2023-08-10 00:06:11 +0530

    Modified the programe little bit!

--- K-Map_Solver.c
+++ K-Map_Solver.c
@@ -2,27 +2,33 @@
 #include <conio.h>
 #include <string.h>
 void Expression(char *p,char *r){
-  if(*p == '1'){
-   strcpy(r,"A' ^ B' + ");
-   r+=10;
+  for(int i=0;i<4;i++){
+  if(*(p+i) == '1'){
+    if(i>=1){
+    strcpy(r," V ");
+    r+=3;
+    }
+    if(i==0){
+    strcpy(r,"(A' ^ B')");
+    r+=9;
+    }
+    if(i==1){
+    strcpy(r,"(A' ^ B)");
+    r+=8;
+    }
+    if(i==2){
+    strcpy(r,"(A ^ B')");
+    r+=8;
+    }
+    if(i==3){
+    strcpy(r,"(A ^ B)");
+    r+=7;
+    }
   }
-  p++;
-  if(*p == '1'){
-    strcpy(r,"A ^ 'B + ");
-   r+=10;
-  }
-  p++;
-  if(*p == '1'){
-    strcpy(r,"A ^ B' + ");
-   r+=10;
-  }
-  p++;
-  if(*p == '1'){
-    strcpy(r,"A ^ B ");
-  }
+  } 
   };
 void main(){
-char Y[2][2]={{'1','0'},{'0','1'}},Result[30];
+char Y[2][2]={{'1','0'},{'1','1'}},Result[30];
 Expression(&Y[0][0],&Result[0]);
 puts(Result);
 getch();
