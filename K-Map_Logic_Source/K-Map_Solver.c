ca028d8a3afe8065efbe9836eeb7870a04777d8b
@Neeraj395-N3rd <nsinghh.04@gmail.com>
2023-09-03 22:48:03 +0530

    Nothing!!!

--- K-Map_Logic_Source/K-Map_Solver.c
+++ K-Map_Logic_Source/K-Map_Solver.c
@@ -3,55 +3,66 @@
 #include <string.h>
 #include "Self_Define_Functions.h"
 
-void main(){
-char *MT=(char *)malloc(5);
-char Result[30]={'\0'};
-int Min_in_Bin[4][2];
-int Stage_1_Output[4][2];
-int row[4];
-int counter;
+void main()
+{
+  char *MT = (char *)malloc(5);
+  char Result[30] = {'\0'};
+  int Min_in_Bin[4][2];
+  int Stage_1_Output[4][2];
+  int row[4];
+  int counter;
 
-printf("Enter the Minterms for Two Varaible K-Map in the respective order\nm0, m1, m2, m3:");
-gets(MT);
+  printf("Enter the Minterms for Two Varaible K-Map in the respective order\nm0, m1, m2, m3:");
+  scanf("%s", MT);
 
-memset(Result,0,sizeof(Result));
-memset(Min_in_Bin,0,sizeof(Min_in_Bin));
-memset(row,0,sizeof(row));
+  memset(Result, 0, sizeof(Result));
+  memset(Min_in_Bin, 0, sizeof(Min_in_Bin));
+  memset(row, 0, sizeof(row));
 
-BitSetCheck(MT,&Min_in_Bin[0][0],4,2);
+  BitSetCheck(MT, &Min_in_Bin[0][0], 4, 2);
 
-if(strcmp(MT,"0000")==0){
-  strcpy(Result,"Logic 0");
-}
+  if (strcmp(MT, "0000") == 0)
+  {
+    strcpy(Result, "Logic 0");
+  }
 
-else if(strcmp(MT,"1111")==0){
-  strcpy(Result,"Logic 1");
-}
+  else if (strcmp(MT, "1111") == 0)
+  {
+    strcpy(Result, "Logic 1");
+  }
 
-else{
-if(strcmp(MT,"1001")==0||strcmp(MT,"0110")==0||Minterm_Counter(&Min_in_Bin[0][0],4,2,&row[0])==1){
-        memcpy(Stage_1_Output,Min_in_Bin,sizeof(Min_in_Bin));
-        free(MT);
-}
-else{
-    for (int N = 0; N < 3; N++) {
-         for (int M = 0; M < 4; M++){
-            if((N==0&&(M==1||M==2))||((N==1||N==2)&&M==3)){
-              Comparator(Min_in_Bin,Stage_1_Output,N,M);
+  else
+  {
+    if (strcmp(MT, "1001") == 0 || strcmp(MT, "0110") == 0 || Minterm_Counter(&Min_in_Bin[0][0], 4, 2, &row[0]) == 1)
+    {
+      memcpy(Stage_1_Output, Min_in_Bin, sizeof(Min_in_Bin));
+      free(MT);
+    }
+    else
+    {
+      for (int N = 0; N < 3; N++)
+      {
+        for (int M = 0; M < 4; M++)
+        {
+          if ((N == 0 && (M == 1 || M == 2)) || ((N == 1 || N == 2) && M == 3))
+          {
+            Comparator(Min_in_Bin, Stage_1_Output, N, M);
+          }
+        }
       }
     }
-  }
-}
 
-if(Minterm_Counter(&Min_in_Bin[0][0],4,2,&row[0])==1){
-  Boolean_Output(&Stage_1_Output[0][0],2,&row[0],&Result[0]);
-}
-else{
-memset(row,0,sizeof(row));
-Minterm_Counter(&Stage_1_Output[0][0],4,2,&row[0]);
-Boolean_Output(&Stage_1_Output[0][0],2,&row[0],&Result[0]);
-}
-}
-puts(Result);
-getch();
+    if (Minterm_Counter(&Min_in_Bin[0][0], 4, 2, &row[0]) == 1)
+    {
+      Boolean_Output(&Stage_1_Output[0][0], 2, &row[0], &Result[0]);
+    }
+    else
+    {
+      memset(row, 0, sizeof(row));
+      Minterm_Counter(&Stage_1_Output[0][0], 4, 2, &row[0]);
+      Boolean_Output(&Stage_1_Output[0][0], 2, &row[0], &Result[0]);
+    }
+  }
+  puts(Result);
+  getch();
 }
--- K-Map_Logic_Source/Self_Define_Functions.h
+++ K-Map_Logic_Source/Self_Define_Functions.h
@@ -3,98 +3,119 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-int Minterm_Counter(int *Minterms,int sor,int soc,int *row){
-  int count=0;
-  for(int i=0;i<sor;i++){
-      if(*(Minterms + (i * soc))!=2){
-        *(row++)=i;
-        count++;
+int Minterm_Counter(int *Minterms, int sor, int soc, int *row)
+{
+  int count = 0;
+  for (int i = 0; i < sor; i++)
+  {
+    if (*(Minterms + (i * soc)) != 2)
+    {
+      *(row++) = i;
+      count++;
     }
   }
   return count;
 }
 
-
-void BitSetCheck(char *Min,int *Min_in_Bin,int sor,int soc){
-int Bit;
-  for(int i = 0; i < sor; i++){
-    if(*(Min++) == '1'){
-      Bit=i;
-      for(int j = (soc-1); j >= 0; j--){
-        *(Min_in_Bin + (i * soc) + j) = Bit%2;
+void BitSetCheck(char *Min, int *Min_in_Bin, int sor, int soc)
+{
+  int Bit;
+  for (int i = 0; i < sor; i++)
+  {
+    if (*(Min++) == '1')
+    {
+      Bit = i;
+      for (int j = (soc - 1); j >= 0; j--)
+      {
+        *(Min_in_Bin + (i * soc) + j) = Bit % 2;
         Bit >>= 1;
+      }
     }
-  }
-  else{
-    for(int j = (soc-1); j >= 0; j--){
+    else
+    {
+      for (int j = (soc - 1); j >= 0; j--)
+      {
         *(Min_in_Bin + (i * soc) + j) = 2;
+      }
     }
   }
 }
-}
 
-
-void Comparator(int Minterms[][2],int Stage_1[][2],int MinPair_N,int MinPair_M){
-  static int Row=0;
-  int checker=0;
-  for(int i = 0; i < 2; i++){
-    if(Minterms[MinPair_N][i]==2||Minterms[MinPair_M][i]==2){
-      Stage_1[Row][i]=2;
+void Comparator(int Minterms[][2], int Stage_1[][2], int MinPair_N, int MinPair_M)
+{
+  static int Row = 0;
+  int checker = 0;
+  for (int i = 0; i < 2; i++)
+  {
+    if (Minterms[MinPair_N][i] == 2 || Minterms[MinPair_M][i] == 2)
+    {
+      Stage_1[Row][i] = 2;
     }
-    else if(checker==1){
-      Stage_1[Row][i]=0;
+    else if (checker == 1)
+    {
+      Stage_1[Row][i] = 0;
     }
-    else if(checker==0 && (Minterms[MinPair_N][i])^(Minterms[MinPair_M][i])==1){
-      Stage_1[Row][i]=4;
+    else if (checker == 0 && (Minterms[MinPair_N][i]) ^ (Minterms[MinPair_M][i]) == 1)
+    {
+      Stage_1[Row][i] = 4;
     }
-    else{
-      Stage_1[Row][i]=Minterms[MinPair_N][i];
+    else
+    {
+      Stage_1[Row][i] = Minterms[MinPair_N][i];
     }
   }
   Row++;
 }
 
-
-void Boolean_Output(int *Minterms,int soc,int *row,char *result){
-    do{
-      *result='(';
-        result++; 
-        for(int j = 0; j < soc; j++){
-            if(*(Minterms + ((*row) * soc) + j)==1){ 
-                snprintf(result,sizeof(result),"%cx",65+j);
-                result++;
-            }
-            else if(*(Minterms + ((*row) * soc) + j)==0){
-                snprintf(result,sizeof(result),"%c'x",65+j);
-                result+=2;
-            }
-            if (*result=='x')
-            {
-            strcpy(result," ^ ");
-            result+=3;
-            }
-        }
-        if(*(result-2)=='^') result-=3;
-        strcpy(result,")  ");
+void Boolean_Output(int *Minterms, int soc, int *row, char *result)
+{
+  do
+  {
+    *result = '(';
+    result++;
+    for (int j = 0; j < soc; j++)
+    {
+      if (*(Minterms + ((*row) * soc) + j) == 1)
+      {
+        snprintf(result, sizeof(result), "%cx", 65 + j);
         result++;
-        ++row;
-        if(*row!=0 && *result!='\0'){
-        strcpy(result," V ");
-        result+=3;
-        }
-        else{ 
-        *result='\0';
-        break;
-        }
-    }while(*row !=0);
+      }
+      else if (*(Minterms + ((*row) * soc) + j) == 0)
+      {
+        snprintf(result, sizeof(result), "%c'x", 65 + j);
+        result += 2;
+      }
+      if (*result == 'x')
+      {
+        strcpy(result, " ^ ");
+        result += 3;
+      }
+    }
+    if (*(result - 2) == '^')
+      result -= 3;
+    strcpy(result, ")  ");
+    result++;
+    ++row;
+    if (*row != 0 && *result != '\0')
+    {
+      strcpy(result, " V ");
+      result += 3;
+    }
+    else
+    {
+      *result = '\0';
+      break;
+    }
+  } while (*row != 0);
 }
 
-
-void ArrayDisplay(int *Array,int sor,int soc,char ArrayName[10]){
-  for(int i = 0; i < sor; i++){
-    for(int j = 0; j < soc; j++){
-      printf("%s[%d][%d] = %d, ",ArrayName,i,j,*(Array + i * soc + j));
+void ArrayDisplay(int *Array, int sor, int soc, char ArrayName[10])
+{
+  for (int i = 0; i < sor; i++)
+  {
+    for (int j = 0; j < soc; j++)
+    {
+      printf("%s[%d][%d] = %d, ", ArrayName, i, j, *(Array + i * soc + j));
     }
     printf("\n");
   }
