bf49f98b915ddf5a49db69acdf47b384ed835893
priyanshu garg <115003186+priyanshugarg00@users.noreply.github.com>
2023-09-27 21:45:10 +0530

    Program Output to MathJax!! (#3)

--- Web_Source/main.js
+++ Web_Source/main.js
@@ -32,3 +32,32 @@ function call(id) {
       document.getElementById(id).innerHTML='0';
     }
 }
+var abar="\\"+"["+"\\"+"bar{A}"+"\\"+"]";
+var bbar="\\"+"["+"\\"+"bar{B}"+"\\"+"]";
+var cbar="\\"+"["+"\\"+"bar{C}"+"\\"+"]";
+var dbar="\\"+"["+"\\"+"bar{D}"+"\\"+"]";
+var input=["(","A'","^","B",")","v","(","A","^","B'",")","v","(","C'","^","D",")","v","(","C","^","D'",")"];
+var Min=getElementById('res85');
+for (let i = 0; i < 1000; i++) {
+  if (input[i] == "A'") {
+    input[i] = abar;
+  }
+  if (input[i] == "B'") {
+    input[i] = bbar;
+  }
+
+  if (input[i] == "C'") {
+    input[i] = cbar;
+  }
+  if (input[i] == "D'") {
+    input[i] = dbar;
+  }
+  if (input[i] == "^") {
+    input[i] = ".";
+  }
+  if (input[i] == "v") {
+    input[i] = "+";
+  }
+}
+Min=input;
+
