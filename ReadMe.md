9c72df3fa05eeeb4f6f283026b2c8434236f07ce
@Neeraj395-N3rd <nsinghh.04@gmail.com>
2023-12-13 23:04:35 +0530

    nothing just a conflict!!

---
+++ ReadMe.md
@@ -0,0 +1,73 @@
+# Karnaugh Map (K-Map) Solver Web Application
+
+Welcome to the Karnaugh Map (K-Map) Solver Web Application repository! This web application allows you to simplify Boolean expressions using K-Maps for up to 4 variables. It has both a backend written in C language and a frontend built with HTML, CSS, and JavaScript.
+
+![image](https://github.com/Neeraj395-N3rd/K-Map_Solver/assets/141737975/10ddef5a-5344-484e-8dda-05f925798847)
+
+***Thier is still some features that will be added soon.***
+
+## Features
+
+- Solve Boolean expressions using Karnaugh Maps.
+- Supports up to 4 variables.
+- Easy-to-use web interface.
+- Backend implemented in C for efficient computation.
+
+## Getting Started
+
+These instructions will help you set up and run the K-Map Solver Web Application on your local machine.
+
+### Prerequisites
+
+- PHP installed on your system.
+- Visual studio redistributable C++ 2019 or newer version
+
+### Installation and Usage
+
+1. Clone this repository to your local machine, or you can also go with the zip code:
+
+   ```bash
+   https://github.com/Neeraj395-N3rd/K-Map_Solver.git
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd K-Map_Solver
+
+3. Start a PHP server in the Web_Source directory:
+
+   ```bash
+   php -S localhost:1000
+
+4. Open your browser and visit:
+
+   ```bash
+   http://localhost:1000/index.html
+
+## Contributing
+if you want to contribute to this project, please follow these steps:
+1. Fork this repository
+2. Create a new branch: `git checkout -b feature/new-feature`.
+3. Make your changes and commit them: `git commit -m 'Add new feature'`.
+4. Push to the branch: `git push origin feature/new-feature`.
+5. Create a pull request.
+
+## License
+This project is licensed under the MIT License - see the [License](LICENSE) file for details. 
+
+## Organization:
+
+![logo](https://github.com/Neeraj395-N3rd/K-Map_Solver/assets/141737975/07fe5d08-8d23-4a6f-b225-40375c78d0f5)
+
+## Note
+This project is intended for educational purposes and as a demonstration of integrating C programming with web technologies. It is not meant for deployment on the internet.
+
+## Contact
+If you have any questions or suggestions, feel free to contact us at `nsinghh.04@gmail.com`
+
+## Developed by
+* [Priyanshu Garg](https://github.com/priyanshugarg00 "Priyanshu Garg")
+* [Neeraj Singh](https://github.com/Neeraj395-N3rd "Neeraj Singh")
+* [Sudhanshu Kaul](https://github.com/Sudhanshukaul123 "Sudhanshu Kaul")
+* [Rahul_Kumar_Tiwari](https://github.com/rahul07tiwari "Rahul Tiwari")
+
---
+++ Source_Code/Quine_McCluskey.h
@@ -0,0 +1,143 @@
+#ifndef QUINE_MCCLUSKEY_H
+#define QUINE_MCCLUSKEY_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+char **_2DPointer(int Rows, int Col);
+void _2DFreeArray(char **Array, int Rows);
+void Dec_to_Bin(char const *MinC, int sor, int soc, char **MinB);
+int CC(char const *MinC, int sor, char ch0, char ch1);
+bool DE(char **MinB, int soc, int M, int diff_posi);
+int DC(char **MinB, int soc, int M, int N, int *diff_posi);
+int PT_Generator(char **MinB, int sor, int soc, int *PP, char Tick[]);
+int Prime_Implicant(char **MinB, char *PI, int soc, char Tick[], int soT);
+
+//----------------Decimal to Binary Coverter-------------------//
+void Dec_to_Bin(char const *MinC, int sor, int soc, char **MinB)
+{
+  int Bit;
+  for (int i = 0, Row = 0; i < sor; i++)
+  {
+    if ((MinC[i] == '1') || (MinC[i] == 'X'))
+    {
+      Bit = i;
+      for (int j = (soc - 1); j >= 0; j--)
+      {
+        MinB[Row][j] = (char)(48 + (Bit % 2));
+        Bit >>= 1;
+      }
+      Row++;
+    }
+  }
+}
+
+//----------------Character-Counter--------------------------//
+int CC(char const *MinC, int sor, char ch0, char ch1)
+{
+  int count = 0;
+  for (int i = 0; i < sor; i++)
+    if (MinC[i] == ch0 || MinC[i] == ch1)
+      count++;
+  return count;
+}
+
+//-------------------Duplication_Eliminator----------------//
+bool DE(char **MinB, int soc, int M, int diff_posi)
+{
+  for (int i = 0; i < soc; i++)
+  {
+    if (MinB[M][i] == '_')
+    {
+      if (diff_posi > i)
+        return false;
+    }
+  }
+  return true;
+}
+
+//-----------------------Diffs_Counter-------------------------//
+int DC(char **MinB, int soc, int M, int N, int *diff_posi)
+{
+  int diff_count = 0;
+  for (int i = 0; i < soc; i++)
+  {
+    if (MinB[M][i] != MinB[N][i])
+    {
+      diff_count++;
+      *diff_posi = (int)i;
+    }
+  }
+  return diff_count;
+}
+
+//------------------------Pair & Tick Generator--------------------------//
+int PT_Generator(char **MinB, int sor, int soc, int *PP, char Tick[])
+{
+  int PProw = 0, diff_posi;
+  for (int M = 0; M < sor; M++)
+  {
+    for (int N = (M + 1); N < sor; N++)
+    {
+      if (DC(MinB, soc, M, N, &diff_posi) == 1)
+      {
+        if (DE(MinB, soc, M, diff_posi))
+        {
+          *(PP + (PProw * 2)) = M;
+          *(PP + (PProw * 2) + 1) = N;
+          PProw++;
+        }
+        Tick[M] = '1';
+        Tick[N] = '1';
+      }
+    }
+  }
+  return PProw;
+}
+
+//-------------------Prime_Implicant_Collector------------------//
+int Prime_Implicant(char **MinB, char *PI, int soc, char Tick[], int soT)
+{
+  static int Line = 0;
+  int i = 0;
+  while ((Tick[i] != '$') && (i != soT))
+  {
+    if (Tick[i] == '0')
+    {
+      strncpy(&PI[Line * soc], &MinB[i][0], sizeof(char) * soc);
+      Line++;
+    }
+    i++;
+  }
+  return Line;
+}
+
+//---Initializing Dynamic_2D-Array----//
+char **_2DPointer(int Rows, int Col)
+{
+  if (Rows < 1 || Col < 1)
+    return NULL;
+  char **Array;
+  Array = (char **)malloc(Rows * sizeof(char *));
+
+  for (int i = 0; i < Rows; i++)
+    Array[i] = (char *)malloc(Col * sizeof(char));
+
+  return Array;
+}
+
+//--------Free The Allocated Array-------//
+void _2DFreeArray(char **Array, int Rows)
+{
+  for (int i = 0; i < Rows; i++)
+    if (Array[i] != NULL)
+      free(Array[i]);
+
+  if (Array != NULL)
+    free(Array);
+}
+//________________END__________________//
+
+#endif
